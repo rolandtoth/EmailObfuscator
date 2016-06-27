a431622c07a6e8de85b34a519a62538be01807d7
Roland Toth <contact@rolandtoth.hu>
2016-06-27 10:29:15 +0200

    jQuery-free version

--- EmailObfuscator.js
+++ EmailObfuscator.js
@@ -1,14 +1,17 @@
-$(function() {
-	// restore mailto: links
-	$("a[href^='GOSPAM:']").each(function(){
-		var email = $(this).attr("href").substr(7);
-		// replace email with its reversed version
-		email = email.split("").reverse().join("");
-		// replace "GOSPAM:" with "mailto:"
-		$(this).attr("href","mailto:" + email);			
-	}); 
-	
-	$(".email-obfuscator-unreverse").each(function() {
-		$(this).replaceWith($(this).text().split("").reverse().join(""));
-	});
+document.addEventListener('DOMContentLoaded', function () {
+
+    // restore mailto: links
+    Array.prototype.forEach.call(document.querySelectorAll('a[href^="GOSPAM:"]'), function (el) {
+
+        var email = el.getAttribute('href').substr(7);
+        // replace email with its reversed version
+        email = email.split('').reverse().join('');
+        // replace "GOSPAM:" with "mailto:"
+        el.setAttribute('href', 'mailto:' + email);
+    });
+
+    Array.prototype.forEach.call(document.querySelectorAll('.email-obfuscator-unreverse'), function (el) {
+        el.innerHTML = el.innerHTML.split('').reverse().join('');
+        el.removeAttribute('style');
+    });
 });
